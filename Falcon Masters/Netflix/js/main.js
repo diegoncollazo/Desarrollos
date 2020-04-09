97f499c6f8f3bcc8f926fe1281f25ebec37d6413
Diego Collazo <diego.nicolas@outlook.com.ar>
2020-04-09 06:24:03 -0300

    Netflix

--- Falcon Masters/Netflix/js/main.js
+++ Falcon Masters/Netflix/js/main.js
@@ -1,17 +1,27 @@
 "use strict";
 const fila = document.querySelector(".contenedor-carousel");
-const pelicula = document.querySelectorAll(".pelicula");
+const peliculas = document.querySelectorAll(".pelicula");
 const flechaIzquierda = document.getElementById("flecha-izquierda");
 const flechaDerecha = document.getElementById("flecha-derecha");
 
 flechaDerecha.addEventListener("click", () => {
 	fila.scrollLeft += fila.offsetWidth;
+	const indicadorActivo = document.querySelector(".indicadores .activo");
+	if (indicadorActivo.nextSibling) {
+		indicadorActivo.nextSibling.classList.add("activo");
+		indicadorActivo.classList.remove("activo");
+	}
 });
 flechaIzquierda.addEventListener("click", () => {
 	fila.scrollLeft -= fila.offsetWidth;
+	const indicadorActivo = document.querySelector(".indicadores .activo");
+	if (indicadorActivo.previousSibling) {
+		indicadorActivo.previousSibling.classList.add("activo");
+		indicadorActivo.classList.remove("activo");
+	}
 });
 
-const numeroPaginas = Math.ceil(pelicula.length / 5);
+const numeroPaginas = Math.ceil(peliculas.length / 5);
 for (let index = 0; index < numeroPaginas; index++) {
 	const indicador = document.createElement("button");
 	document.querySelector(".indicadores").appendChild(indicador);
@@ -27,6 +37,19 @@ for (let index = 0; index < numeroPaginas; index++) {
 	});
 }
 
+peliculas.forEach((pelicula) => {
+	pelicula.addEventListener("mouseenter", (e) => {
+		const elemento = e.currentTarget;
+		setTimeout(() => {
+			peliculas.forEach((pelicula) => pelicula.classList.remove("hover"));
+			elemento.classList.add("hover");
+		}, 300);
+	});
+});
+
+fila.addEventListener('mouseleave', () => {
+	peliculas.forEach((pelicula) => pelicula.classList.remove("hover"));
+});
 // button:active {
 //     outline: none;
 //     border: none;
